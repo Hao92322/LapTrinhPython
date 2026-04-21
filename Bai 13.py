b6bef8626946d5e4a5b1c6df056e7577b8cd3c18
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-04-21 09:56:50 +0700

    Cap nhat lai Bai 13

--- Bai 13.py
+++ Bai 13.py
@@ -3,8 +3,9 @@ import re
 def chuan_hoa_va_dem(chuoi):
     # cat khoang trang thua dau va cuoi chuoi
     chuoi = chuoi.strip()
+    chuoi = re.sub(r'([.,])\1+', r'\1', chuoi)
     # xu ly cho dau cham dau phay sat vao tu
-    chuoi = re.sub(r'\s+([.,])', r'\1', chuoi)
+    chuoi = re.sub(r'\s*([.,])\s*', r'\1', chuoi)
      # cat khoang trang giua cac tu
     chuoi = re.sub(r'\s+', ' ', chuoi)
     
