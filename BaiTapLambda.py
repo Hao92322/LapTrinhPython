0cc703fcb506717fccb8d3db6421fe5760b2ec02
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-04-24 09:59:07 +0700

    Them Bai Tap Thuc Hanh su dung Lambda

---
+++ BaiTapLambda.py
@@ -0,0 +1,12 @@
+#a
+GiaTriTuyetDoi = lambda N : abs(N)
+#b
+SoNguyenCong15 = lambda N : N + 15
+#c
+Tich2So = lambda x,y : x*y
+#d
+BoiSo13va19 = lambda n : 'n la boi so cua 13 hoac 19' if n % 13 == 0 or n % 19 == 0 else 'n khong phai boi so cua 13 hoac 19'
+#e
+DtHinhtron = lambda r : 3.14 * r**r
+#f
+DtHinhChuNhat = lambda d,r : d*r
