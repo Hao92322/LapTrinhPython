f7395a2e79beab1a103540040ffb22970ae0d1e3
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-04-24 11:22:28 +0700

    Them bai 5 bai tap de quy su dung python

--- BaiTapDeQuy.py
+++ BaiTapDeQuy.py
@@ -10,11 +10,18 @@ def GiaiThua(n):
     return GiaiThua(n-1)*n
 #3
 def LuyThua(a,b):
-    if(b==1):
+    if(b == 0):
+        return 1
+    if(b == 1):
         return a
     return LuyThua(a,b-1)*a
 #4
 def UCLN(a,b):
     if(b == 0):
        return a
-    return UCLN(b,a%b)
+    return UCLN(b,a%b) # a va b chia het cho uoc chung lon nhat thi a%b cung chia het cho uoc chung lon nhat
+#5
+def Fibonaci(n):
+    if(n<=2):
+        return 1
+    return Fibonaci(n-2) + Fibonaci(n-1)
