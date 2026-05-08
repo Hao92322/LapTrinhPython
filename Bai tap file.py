92442914fa60e0fedbfa5d59469a9fadd3926b9d
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-05-08 12:08:49 +0700

    To do

---
+++ Bai tap file.py
@@ -0,0 +1,12 @@
+import os
+file = open('test2.txt',mode='r',encoding="utf-8")
+dict = ()
+list = []
+col = 0
+row = 0
+for line in file:
+    s = line.strip().split()
+    for word in s:
+        dict
+print(dict)
+filedagiamdungluong = open('test2GiamDungLuong.txt',mode='w',encoding='utf-8')
