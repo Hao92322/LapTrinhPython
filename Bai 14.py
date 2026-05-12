70b54c8b1abfa97defb2af6d3f89816b684c5167
Hao92322 <nguyenkhagiahao10@gmail.com>
2026-05-12 10:01:00 +0700

    Cap nhat Bai14.py

--- Bai 14.py
+++ Bai 14.py
@@ -1,17 +1,16 @@
 Money = [1,2,5,10,20,50,100,200,500]
-Moneytranfer = [0]*10
+SumMoney = 0
 try:
     moneyinput = int(input())
     print(f'X = {moneyinput} : ')
-    while moneyinput > 0:
-        for i in range(len(Money)-1,0,-1):  
-            if moneyinput // Money[i] > 0:
-                Moneytranfer[i]= moneyinput // Money[i]
-            moneyinput = moneyinput % Money[i]
-    for i in range(len(Moneytranfer)-1,0,-1):
-        if Moneytranfer[i] > 0:
-            print(f'Loai {Money[i]} gom {Moneytranfer[i]} to')
-    print(f'TONG CONG CO {sum(Moneytranfer)} TO')
+    for i in range(len(Money)-1,0,-1):  
+        if moneyinput // Money[i] > 0:
+            #Moneytransfer[i]= moneyinput // Money[i]
+            SumMoney += moneyinput//Money[i]
+            print(f'Loai {Money[i]} gom {moneyinput//Money[i]} to')
+        moneyinput = moneyinput % Money[i]
+    
+    print(f'TONG CONG CO {SumMoney} TO')
 except:
     print("Sai dinh dang")
     
